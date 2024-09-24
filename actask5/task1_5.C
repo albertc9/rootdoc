a6cb1618c734fc3c3c9a278b98f34a3cecb301d2
albert_fe <albert.chg9@gmail.com>
2024-09-24 22:08:47 +0800

    20240924

--- actask5/task1_5.C
+++ actask5/task1_5.C
@@ -1,5 +1,7 @@
-// 有一个需要注意的点：在ROOT中，直方图默认与当前目录关联（例如TFile）。当关闭TFile时，与其关联的所有直方图都会被删除，如果之后需要调用的话就会产生无效的内存访问。这可能是我在运行中偶尔出现 crash 的原因。
-// gSystem -> Load("libMathCore"); // 似乎有时会产生崩溃，并且提示可能问题出在 libMathCore 上。因此添加这行代码防止崩溃（虽然可能是我电脑的问题）。
+/* 
+* 注意1：在ROOT中，直方图默认与当前目录关联（例如TFile）。当关闭TFile时，与其关联的所有直方图都会被删除，如果之后需要调用的话就会产生无效的内存访问。这可能是我在运行中偶尔出现 crash 的原因。
+* 注意2：在所有部分，我只计算了区间（45,80）的卡方/自由度。
+*/
 
 #include "langaufun.C" // 该函数是根据cern.root指导网页中教程中得到的，其中定义了郎道卷积高斯函数
 
@@ -7,7 +9,7 @@ void task1_5() {
 	auto start = std::chrono::high_resolution_clock::now(); // 记录程序运行时间
 
     TVirtualFitter::SetDefaultFitter("Minuit"); // **在上一次程序运行中发现无法使用 Minuit 改用 Minuit2，这一次发现无法使用 Minuit2 改用 Minuit。由于每个A_B都要提示一次（1152次），于是预先在这里声明。但是为什么？**
-    TFile *file = TFile::Open("va.root", "READ");
+    TFile *file = TFile:: Open("va.root", "READ");
     TFile *outFile = new TFile("fit_results.root","RECREATE");
 
     std::ofstream outputFile("chi2Ndf_and_gainCoefficients.txt");
@@ -22,9 +24,13 @@ void task1_5() {
     std::vector<TH1D*> correctedHists; // 输出的（校正后的）值方图
     std::vector<TH1D*> originalHists; // 由于出现了程序崩溃，存储一个原始直方图。应该是程序开头的注释中提到的问题
 
-    int lefFit = 0, riFit = 0; // 拟合区间
-    lefFit = 50;
-    riFit = 100;
+    int lefFit = 0, riFit = 0, lefMergedFit = 0, riMergedFit = 0; // 拟合区间
+    lefFit = 45;
+    riFit = 75;
+    lefMergedFit = 45;
+    riMergedFit = 75;
+
+
 
     for (int A=0; A < 192; ++A){
         for (int B=0; B < 6; ++B){
@@ -46,8 +52,18 @@ void task1_5() {
 
             double gainCoefficient = landauGausFit -> GetMaximumX(lefFit, riFit);
 
-            double chi2 = landauGausFit -> GetChisquare();
-            int ndf = landauGausFit -> GetNDF();
+            double chi2 = 0;
+            int ndf = 0;
+            for (int bin = hist -> FindBin(45); bin <= hist -> FindBin(90); bin ++){
+            double observed = hist -> GetBinContent(bin);
+            double expected = landauGausFit -> Eval(hist ->GetBinCenter(bin));
+            if (expected > 0){
+                chi2 += (observed -expected) * (observed - expected)/ expected;
+                ndf++; 
+            }
+            }
+            ndf -= 4;
+
             double chi2NDF = chi2 / ndf;
 
             gainMap -> SetBinContent(A + 1, B + 1, gainCoefficient);
@@ -93,11 +109,25 @@ void task1_5() {
         mergedHist -> Add(correctedHists[i]);
     }
 
-    TF1 *mergedFit = new TF1("mergedFit", langaufun, lefFit, riFit, 4);
+    TF1 *mergedFit = new TF1("mergedFit", langaufun, lefMergedFit, riMergedFit, 4);
     mergedFit -> SetParameters(1.8, 50, 50000, 3);
     mergedFit -> SetParNames("Width", "MP", "Area", "Sigma");
     mergedFit -> SetNpx(1000);
     mergedHist -> Fit(mergedFit, "Q");
+
+    double mergedChi2 = 0;
+    int mergedNDF = 0;
+    for (int bin = mergedHist -> FindBin(45); bin <= mergedHist -> FindBin(80);bin ++){
+        double observed = mergedHist -> GetBinContent(bin);
+        double expected = mergedFit -> Eval(mergedHist -> GetBinCenter(bin));
+        if (expected > 0){
+            mergedChi2 += (observed - expected) * (observed - expected) / expected;
+            mergedNDF++;
+        }
+    }
+    mergedNDF -= 4;
+    
+    double mergedChi2NDF = mergedChi2 / mergedNDF;
 
     mergedFit -> Write();
     mergedHist -> Write();
@@ -115,6 +145,7 @@ void task1_5() {
     TH2D *chi2Hist = (TH2D*)inFile -> Get("chi2Map"); // 卡方分布热力图
     TH1D *mergedHistFromFile = (TH1D*)inFile -> Get("merged_hist"); // 合并后的直方图
     TF1 *mergedFitFromFile = (TF1*)inFile -> Get("mergedFit"); // 拟合后的合并后的直方图
+    TH1D *gainHist1D = new TH1D("gainHist1D", "Gain Coefficients; Detector ID; Gain Coefficient", 1152, 1, 1153); // 1152 bin的直方图，用于绘制增益系数
 
     TCanvas *c1 = new TCanvas("c1", "Gain Coefficient Heatmap", 800, 600);
     gStyle -> SetOptStat(0);
@@ -148,18 +179,29 @@ void task1_5() {
     stats -> SetX2NDC(0.90);
     stats -> SetY2NDC(0.90);
     stats -> SetBorderSize(1);
+    stats -> AddText(TString::Format("Chi2/NDF = %.2f", mergedChi2NDF));
     }
 
-    c4 -> SaveAs("fitted_merged_spectrum.pdf");
+    c4 -> SaveAs("fitted_merged_spectrum.pdf");  
+    TCanvas *c5 = new TCanvas("c5", "Gain Coefficients 2D plot", 800, 600);
+    for (int i = 0; i < 1152; ++i){
+        gainHist1D -> SetBinContent(i+1, mpvList[i]);
+    }
+    gainHist1D -> Draw("HIST");
+
+    c5 -> SaveAs("gain_coefficients_1D_plot.pdf");
 
     inFile -> Close(0);
     c1 -> Close(0);
     c2 -> Close(0);
     c3 -> Close(0);
     c4 -> Close(0);
+    c5 -> Close(0);
 
 	auto end = std::chrono::high_resolution_clock::now();
-
 	std::chrono::duration<double> elapsed = end - start;
-std::cout << "Total execution time: " << elapsed.count() << " seconds." << std::endl;
+
+    std::cout << "Total execution time: " << elapsed.count() << " seconds." << std::endl;
+    std::cout << "Attention: All Chi2 / NDF are in the range of [45, 80]." << std::endl;
+
 }
