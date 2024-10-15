7fd1fe531532c71992e1058fc5e6e3b5dfec7834
albert_fe <albert.chg9@gmail.com>
2024-10-15 16:47:19 +0800

    111

--- actask7/pile_up.C
+++ actask7/pile_up.C
@@ -1,52 +1,43 @@
+void pile_up() {
+    // 定义测试时间，单位：毫秒
+    const int all = 2000000;
+    // 堆积事件数量
+    int pile_up = 0;
+    // 记录触发事件数量
+    int Event_all = 0;
 
-void pile_up(){
+    // 泊松分布参数
+    double event_ratio = 60; // 60 Hz
+    // 将泊松分布参数从秒转换为毫秒
+    double lamda = event_ratio * 0.001;
 
-    // define test time, unit: ms
-    const int all=2000000;
-    // {ile-up event ratio
-    int pile_up=0;
-    // record occur event number
-    int Event_all=0;
+    // 记录每个时间点的事件发生情况
+    int event_flag[all] = {0};
+    // 记录每个时间点探测器的工作状态
+    int work_flag[all] = {0};
 
-    // Poisson parameter define
-    double event_ratio=60; // 60 Hz
-    // change Poisson parameter from s to ms;
-    double lamda=event_ratio*0.001;
+    TRandom3 randGen(0);
 
-
-
-    //record event occur event number with time
-    int event_flag[all]={0};
-    //record work state with time
-    int work_flag[all]={0};
-
-    for(int i=0;i<all;++i){
-    int temp=gRandom->Poisson(lamda);
-    if(temp){
-        Event_all++;
-        event_flag[i]=temp;
-    // if detector is not working, change work state in next 3 ms;
-        if(!work_flag[i]){
-        work_flag[i]=1;
-        if (i + 1 < all) work_flag[i + 1]=1;
-        if (i + 2 < all) work_flag[i + 2]=1;
+    for (int i = 0; i < all; ++i) {
+        int temp = randGen.Poisson(lamda);
+        if (temp) {
+            Event_all++;
+            event_flag[i] = temp;
+            // 如果探测器不在死时间内，则更改接下来 3 ms 的工作状态
+            if (!work_flag[i]) {
+                work_flag[i] = 1;
+                if (i + 1 < all) work_flag[i + 1] = 1;
+                if (i + 2 < all) work_flag[i + 2] = 1;
+            }
+            else pile_up++; // 如果在死时间内发生事件，计入堆积事件数
         }
-        else pile_up++;
-    }
     }
 
+    // 输出结果
+    cout << "Event All: " << Event_all << "; Pile-up Event: " << pile_up << "; Time All: " << all << " ms" << endl;
+    // 计算事件堆积率
     double pile_up_rate = pile_up / static_cast<double>(Event_all);
-
-    // output to check
-    /*
-    for(int i=0;i<1000;++i){
-    cout<<event_flag[i]<<"  "<<work_flag[i]<<endl;
-    }
-    */
-
-    cout<<"Event All: "<<Event_all<<"; Pile-up Event "<<pile_up<<"; Time All: "<<all<<" ms"<<endl;
-    // output pile-up ratio && check if the event ratio equal to define ratio
     cout << "Pile-up Rate: " << pile_up_rate << endl;
-    cout<<"pile_up possibility:  "<<pile_up/1./all<<"; Event ratio: "<<Event_all/1./all*1000<<endl;
-
-    }
+    // 检查是否达到了预设的事件率
+    cout << "Event Ratio: " << Event_all / static_cast<double>(all) * 1000 << " Hz" << endl;
+}
